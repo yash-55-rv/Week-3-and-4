bcb026dedec5f40e00ba6cd5476695a483249b9b
Venkat Yashwanth <yashwanthrachamalla2007@gmail.com>
2026-03-15 12:37:05 +0530

    Create LeapYear2.java

---
+++ LeapYear2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class LeapYear2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a year: ");
+        int year = sc.nextInt();
+
+        if (year < 1582) {
+            System.out.println("Year must be 1582 or later.");
+        } else {
+
+            if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+                System.out.println(year + " is a Leap Year");
+            } else {
+                System.out.println(year + " is not a Leap Year");
+            }
+        }
+        sc.close();
+    }
+}
