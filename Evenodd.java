b3c5f2370ec7146bda35b1671926e6a2f0a4b0a2
Venkat Yashwanth <yashwanthrachamalla2007@gmail.com>
2026-03-15 12:24:11 +0530

    Evenodd.java

---
+++ Evenodd.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Evenodd{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a natural number: ");
+        int number = scanner.nextInt();
+
+        if (number <= 0) {
+            System.out.println("Please enter a natural number greater than 0.");
+        } else {
+            for (int i = 1; i <= number; i++) {
+                if (i % 2 == 0) {
+                    System.out.println(i + " is an even number");
+                } else {
+                    System.out.println(i + " is an odd number");
+                }
+            }
+        }
+
+        scanner.close();
+    }
+}
